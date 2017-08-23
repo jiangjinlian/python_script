d73373b0621512b13a3430b5a0a168fc17589727
bytedance <bytedance@bytedancedeMacBook-Pro.local>
2017-08-23 13:59:22 +0800

    delete person formation

--- reminding.py
+++ reminding.py
@@ -13,87 +13,8 @@ from collections import defaultdict
 # from infosys import InfoSysAPI
 
 def get_msg():
-    
-    msg = "Reminding\n\n"
-    
-    url = 'https://jira.bytedance.com/rest/api/2/search?jql=project%20=%20TTINTPGC%20and%20status%20in%20(Open,%20%22In%20Progress%22,%20Reopened)AND%20issuetype%20%3D%20Bug%20'
-    
-    headers = dict()
-    headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30"
-    headers["Cookie"] = "%s; _ga=GA1.2.1001356093.1470803696" % get_session_id()
-    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-            
-    response = requests.get(url, verify=False, headers=headers)
-            
-    data = demjson.decode(response.text)
-            
-    issues = data.get("issues")
-            
-    issue_total_count = data.get("total")
-    msg += "Total: "
-    msg += str(issue_total_count)
-    msg += "\n\n"
-            
-    issue_assignee_count = dict()
-            
-    for issue in issues:
-        assignee = issue.get("fields").get("assignee").get("displayName")
-        if assignee in issue_assignee_count.keys():
-           issue_assignee_count[assignee] += 1
-        else:
-           issue_assignee_count[assignee] = 1
+    pass
 
-    reverse_list = sorted(issue_assignee_count.items(), lambda x, y: cmp(x[1], y[1]), reverse=True)
-    
-    count_sort = defaultdict(list)
-    for key, value in reverse_list:
-        count_sort[value].append(key)
-
-    count_sort_desc = sorted(count_sort.items(), lambda x, y: cmp(x[0], y[0]), reverse=True)
-    score_user_count = 0
-    score_rank = 1
-    
-    # user_score = dict()
-    
-    #with open('user_score.json', 'r') as f:
-    #    user_score = json.load(f)
-    
-    weekday = datetime.datetime.now().weekday()
-    today_date = datetime.date.today().strftime("%Y-%m-%d")
-    for assignee in reverse_list:
-        msg += assignee[0]
-        msg += ": "
-        msg += str(assignee[1])
-        msg += "\n"
-    
-    msg += "\n"
-    if not issues:
-        issue_key = issues[0].get("key")
-    else:
-        issue_key = "TTINTPGC-255"
-    msg += "Click to see the detail  https://jira.bytedance.com/browse/%s?filter=13359" % issue_key
-    # msg += "Click to see the detail  https://jira.bytedance.com/browse/TTINTPGC-77?filter=13359"
-
-    return msg
-
-
-def get_session_id():
-    login_url = "https://jira.bytedance.com/rest/gadget/1.0/login"
-    params = dict()
-    params["os_username"] = "lina.02"
-    params["os_password"] = "ByteDance2016!"
-    
-    headers = dict()
-    headers["X-Requested-With"] = "XMLHttpRequest"
-    headers["Referer"] = "https://jira.bytedance.com/secure/Dashboard.jspa"
-    headers["Cookie"] = "atlassian.xsrf.token=B2HT-9ELJ-5A6E-IWUO|af20ecd1c8ad03125335ec3c2d6d5df7bf7e2f2b|lout; JSESSIONID=1E6A7DB996CF256537C89785B08777C1; _ga=GA1.2.1001356093.1470803696; _gat=1"
-    headers["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.1 Safari/603.1.30"
-    
-    response = requests.post(login_url, headers=headers, params=params, verify=False)
-    set_cookie = response.headers.get("Set-Cookie")
-    set_cookie_new = set_cookie.replace("; Path=/", "").replace("HttpOnly,", "")
-    
-    return set_cookie_new
 
 
 def notify_dd(title=None, msg=None):
