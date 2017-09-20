57ca1becc5553d5c040879c96e264f8ac59a622e
jiangjinlian <30627852+jiangjinlian@users.noreply.github.com>
2017-09-20 11:16:48 +0800

    Update sum_login.py

--- sum_login.py
+++ sum_login.py
@@ -18,8 +18,8 @@ class Test_mail_126_login(unittest.TestCase):
     
     def test_mail_126_login(self):
         self.driver.get("https://cas.shmtu.edu.cn/cas/login?service=http%3A%2F%2Fportal.shmtu.edu.cn%2Fdcp%2Findex.jsp")
-        self.driver.find_element_by_id("username").send_keys('201630310002')
-        self.driver.find_element_by_id('password').send_keys('260788')
+        self.driver.find_element_by_id("username").send_keys('xxxxxxx')
+        self.driver.find_element_by_id('password').send_keys('xxxxx')
         self.driver.find_element_by_class_name('btn-submit').click()
         time.sleep(3)
         self.driver.find_element_by_link_text('here').click()
